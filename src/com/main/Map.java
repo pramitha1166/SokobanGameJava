8fdba88296e8e500797aa9ec8f77ec704ae67677
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2020-10-25 19:57:13 +0530

    Menu Panel update

--- src/com/main/Map.java
+++ src/com/main/Map.java
@@ -2,13 +2,10 @@ package com.main;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class Map extends JPanel {
